5ed2b87f3fb6b0f569d7957736119270c12323c4
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-20 14:42:14 +0800

    Updated server/client test.

--- numric.js
+++ numric.js
@@ -34,10 +34,11 @@
 			"file": "numric.js",
 			"module": "numric",
 			"author": "Richeve S. Bebedor",
-			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-			],
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/numric.git",
 			"test": "numric-test.js",
 			"global": true
@@ -78,7 +79,7 @@ const numric = function numric( value ){
 	if( !protype( value, STRING + NUMBER ) ){
 		throw new Error( "invalid value" );
 	}
-	
+
 	return truly( value ) && NUMERIC_PATTERN.test( stringe( value ) );
 };
 
--- numric.support.js
+++ numric.support.js
@@ -34,10 +34,11 @@
               			"file": "numric.js",
               			"module": "numric",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/numric.git",
               			"test": "numric-test.js",
               			"global": true
